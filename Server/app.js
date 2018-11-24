e5495ef90362da8bd36947caf380e2c82261036f
TRIF3X <thewizardofcrypto@gmail.com>
2018-11-24 16:59:45 -0600

    created new query for author

--- Server/app.js
+++ Server/app.js
@@ -5,7 +5,8 @@ const schema = require('./schema/schema.js')
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-    schema: schema
+    schema: schema,
+   //graphiql: true
 }))
 
 port=9000;
