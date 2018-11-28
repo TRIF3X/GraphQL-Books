97ab418c4265e6b8e1fb744019660cc41601692d
TRIF3X <thewizardofcrypto@gmail.com>
2018-11-27 20:03:54 -0600

    started on front-end, CRA

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div id='main'>
+       <h1>Conner's reading list</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
