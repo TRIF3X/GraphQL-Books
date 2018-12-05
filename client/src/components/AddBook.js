51c94db180d6f71781264e7f1488f3b49e978474
TRIF3X <hoessly.conner@gmail.com>
2018-12-04 20:21:08 -0600

    created state and submit for add book form

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -4,6 +4,14 @@ import { getAuthorsQuery } from '../queries/queries.js'
 
 
  class AddBook extends Component {
+     constructor(props){
+         super(props)
+         this.state = {
+            name: '',
+            genre: '',
+            authorId: ''
+         }
+     }
 
     displayAuthors() {
         let data = this.props.data
@@ -20,21 +28,25 @@ import { getAuthorsQuery } from '../queries/queries.js'
         }
     }
 
-  
-    render() {
+    submitForm(e){
+        e.preventDefault()
+        console.log(this.state)
+    }
+
+   render() {
       return (
-        <form id="add-book">
+    <form id="add-book" onSubmit={this.submitForm.bind(this)}>
         <div className="field">
             <label>Book name:</label>
-            <input type="text" />
+            <input type="text" onChange={(e) => this.setState({name: e.target.value})} />
         </div>
         <div className="field">
             <label>Genre:</label>
-            <input type="text" />
+            <input type="text" onChange={(e) => this.setState({ genre: e.target.value })} />
         </div>
         <div className="field">
             <label>Author:</label>
-            <select>
+            <select onChange={(e) => this.setState({ authorId: e.target.value })}>
                 <option>Select author</option>
                 {this.displayAuthors()}
             </select>
