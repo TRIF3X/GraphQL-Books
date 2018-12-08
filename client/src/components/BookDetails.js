9865898f67547cd6277e0e9dededba0b72c414ac
TRIF3X <thewizardofcrypto@gmail.com>
2018-12-08 13:38:36 -0600

    styled book app

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -12,7 +12,7 @@ class BookDetails extends Component {
                     <h2>{book.name}</h2>
                     <p>{book.genre}</p>
                     <p>{book.author.name}</p>
-                    <p>All books by this author:</p>
+                    <p>More books by this author:</p>
                     <ul className="other-books">
                         {
                             book.author.books.map(item => {
